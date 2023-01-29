7df6b9dcbc30e52faa8619506256905caef98096
Emperor-poof <117320523+Emperor-poof@users.noreply.github.com>
2023-01-28 23:14:53 -0600

    A start

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Basic-Blog-Template
+## in this repository I will give you a free template for a blog
---
+++ src/index.js
@@ -0,0 +1,9 @@
+export function side_bar() {
+    const side = document.getElementById('navat').innerHTML
+    side += `
+    <ul>
+    <li>Home</li>
+    <li>Blog</li>
+    <li>About Me</li>
+    </ul>`
+}
